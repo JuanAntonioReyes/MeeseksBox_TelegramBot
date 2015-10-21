f2ab4179b8bdc2775189ca6fe0ca39b21c1a050f
Juan Antonio Reyes <jantonio.reyes.navarro@gmail.com>
2015-10-22 00:44:37 +0200

    First commit
    General modifications to the original code (main.py), README and LICENSE

---
+++ README.md
@@ -0,0 +1,28 @@
+# SickSanchez-TelegramBot
+TelegramBot that returns random gif or quotes from the cartoon "Rick and Morty" (&copy; Adult Swim)
+
+Modified from the code of [NickCage-TelegramBot](https://github.com/dobladov/NickCage-TelegramBot) by [Daniel Doblado](https://github.com/dobladov) and the quote selection from [Doctor Who Chat Telegram-Bot](https://github.com/Mangu93/DWChatTelebot) by [Adrian Marin](https://github.com/Mangu93)
+
+## Install
+
+		pip install requests giphypop python-telegram-bot
+
+## Server
+
+		python main.py
+
+## Docs
+
++ [Telegram Bot Platform](https://telegram.org/blog/bot-revolution)
++ [Bots: An introduction for developers](https://core.telegram.org/bots)
++ [Telegram Bot API](https://core.telegram.org/bots/api)
++ [Giphy API Python wrapper](https://github.com/shaunduncan/giphypop)
++ [Telegram Bot API Python wrapper](https://github.com/leandrotoledo/python-telegram-bot)
+
+![MRMEESEEKS](http://i.imgur.com/cme3hdj.gif)
+
+
+
+
+
+https://github.com/Mangu93/DWChatTelebot
---
+++ main.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import logging
+import telegram
+import random
+import json
+from giphypop import translate
+
+with open('quotes.json') as data_file:
+    quotes = json.load(data_file)
+    rick = quotes["Rick"]
+    morty = quotes["Morty"]
+
+def main():
+    logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    bot = telegram.Bot(token='HERE GOES THE TOKEN')
+
+    try:
+        LAST_UPDATE_ID = bot.getUpdates()[-1].update_id
+    except IndexError:
+        LAST_UPDATE_ID = None
+
+    while True:
+        for update in bot.getUpdates(offset=LAST_UPDATE_ID, timeout=5):
+            text = update.message.text
+            chat_id = update.message.chat.id
+            update_id = update.update_id
+
+            if '/start' in text:
+                custom_keyboard = [["/quote", "/gif"]]
+                reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, resize_keyboard=True)
+                bot.sendMessage(chat_id=chat_id,
+                                text="I'm Mr. Meeseeks! Look at me!!",
+                                reply_markup=reply_markup)
+                LAST_UPDATE_ID = update_id + 1
+
+            elif '/quote' in text:
+                answer = quote()
+                bot.sendMessage(chat_id=chat_id,
+                                text=answer)
+                LAST_UPDATE_ID = update_id + 1
+
+            elif '/gif' in text:
+                bot.sendMessage(chat_id=chat_id,
+                                text="Ooohhh can do. ")
+                img = translate('rick and morty')
+                bot.sendDocument(chat_id=chat_id,
+                                 document=img.fixed_height.url)
+                print "Enviar Gif " + img.fixed_height.url
+                LAST_UPDATE_ID = update_id + 1
+
+
+def quote():
+    return random.choice(quotes)
+	docnumber = random.randint(0,)
+    if docnumber == 0:
+        return "Rick: " + random.choice(rick)
+    elif docnumber == 1:
+        return "Morty: "  + random.choice(morty)
+
+if __name__ == '__main__':
+    main()
